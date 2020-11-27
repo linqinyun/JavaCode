994b5a098c33880d642b924979888afd084a4b22
1984 <1293244081@qq.com>
2020-11-27 18:14:26 +0800

    freemarker page add servlet

---
+++ fm-web/src/pers/java/freemarker/ListServlet.java
@@ -0,0 +1,43 @@
+package pers.java.freemarker;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListServlet
+ */
+@WebServlet("/list")
+public class ListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		List list = new ArrayList();
+		list.add(new Employee(3301,"张三","市场部","客户代表",8000f));
+		list.add(new Employee(3302,"李四","研发部","运维工程师",7000f));
+		request.setAttribute("employee_list", list);
+
+		request.getRequestDispatcher("/employee.ftl").forward(request, response);
+	}
+
+}
